31cd97c0e95f05436391d49a8d91e2d2a2be05bd
statox <adrien.fabre92@gmail.com>
2015-12-04 18:11:05 -0500

    noms de fichiers finaux

--- TP3/install.sh
+++ TP3/install.sh
@@ -13,11 +13,10 @@ debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/app-pass password root'
 debconf-set-selections <<< 'phpmyadmin phpmyadmin/setup-password password root'    
 debconf-set-selections <<< 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2'    
 
-# -----------------------------------
-
 # installation de mysql-server, mysql-client, apache2,  php5, libapache2-mod-php5, php5-mysql, phpmyadmin. Dans le meme ordre
 
 apt-get -y install mysql-server mysql-client apache2 php5 libapache2-mod-php5 php5-mysql phpmyadmin
 
+#Ajout du fichier de configuration apache
 echo "Include /etc/phpmyadmin/apache.conf" | sudo tee --append /etc/apache2/apache2.conf
 service apache2 restart
